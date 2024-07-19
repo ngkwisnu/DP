6d2eb83fa7139467cceec391012cd5c4b48941ee
Wisnukan <28.ngakanmadewisnumahesaadnyana.com>
2024-07-19 17:11:17 +0800

    Ready

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://dongeng-pustaka.vercel.app/",
     credentials: true,
   })
 );
